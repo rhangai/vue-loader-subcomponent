eb496448ffd3539ce55e033d7cccedf9840a2517
renanhangai <hangai.renan@gmail.com>
2017-03-18 20:29:18 -0300

    0.2.0

--- index.js
+++ index.js
@@ -5,13 +5,17 @@
 }(this, (function () { 'use strict';
 
 function VueLoaderSubcomponent(content) {
+    var haveSubcomponents = false;
     var lines = content.split("\n").map(function (line) {
         if (line.indexOf("vue-loader-subcomponent/subcomponent") >= 0) {
             line = line + "(_vueSubcomponents)";
+            haveSubcomponents = true;
         }
         return line;
     });
-    return "var _vueSubcomponents = {};\n" + lines.join("\n") + "\n;(function() {\nvar components = Component.exports.components = Component.exports.components || {};\nfor ( var name in _vueSubcomponents ) {\n    var c = _vueSubcomponents[ name ];\n\n    // Recurse subcomponents\n    c.components = c.components || {};\n    for ( var subcomp in _vueSubcomponents )\n        c.components[ subcomp ] = _vueSubcomponents[ subcomp ];\n\n    components[ name ] = c;\n    if ( typeof(exports) !== 'undefined' && exports.__esModule )\n        exports[ name ] = c;\n    else\n        module.exports[ name ] = c;\n}\n}());\n";
+    if (!haveSubcomponents) return content;
+
+    return "var _vueSubcomponents = {};\n" + lines.join("\n") + "\n;(function() {\nvar components = Component.exports.components = Component.exports.components || {};\nfor ( var name in _vueSubcomponents ) {\n    var c = _vueSubcomponents[ name ];\n\n    // Recurse subcomponents\n    c.components = c.components || {};\n    for ( var subcomp in _vueSubcomponents )\n        c.components[ subcomp ] = _vueSubcomponents[ subcomp ];\n\n    components[ name ] = c;\n    if ( typeof(exports) !== 'undefined' && exports.__esModule )\n        exports[ name ] = c;\n    else\n        module.exports[ name ] = c;\n}\nvar oldCreate = Component.exports.beforeCreate;\nComponent.exports.beforeCreate = function() {\n    this.$subcomponents = _vueSubcomponents;\n    if ( oldCreate )\n        oldCreate.apply( this );\n};  \n}());\n";
 }
 
 return VueLoaderSubcomponent;
