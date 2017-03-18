a5134d02aa7e6837433a38fd241dd1896baf7006
renanhangai <hangai.renan@gmail.com>
2017-03-18 19:43:29 -0300

    Added es6

--- src/subcomponent.js
+++ src/subcomponent.js
@@ -1,15 +1,19 @@
-var loaderUtils = require( 'loader-utils' );
-var path = require( 'path' );
-module.exports = function( content ) {
-	var query = loaderUtils.parseQuery( this.query );
-	var src  = new Buffer(content, 'utf8').toString('hex');
-	var file = path.join( this.resourcePath, query.name+'.vue' );
+const loaderUtils = require( 'loader-utils' );
+const path        = require( 'path' );
+
+export default function subcomponent( content ) {
+	const query = loaderUtils.parseQuery( this.query );
+	const src  = new Buffer(content, 'utf8').toString('hex');
+	const file = path.join( this.resourcePath, query.name+'.vue' );
 	
-	var name  = query.name;
-	name = name.charAt(0).toUpperCase()+name.substr(1).replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+	const name = capitalize( query.name );
 	return `
 var c = require("virtual-file-loader?src=${src}&file=${file}!");
 module.exports = function(subcomponents) {
 subcomponents[${JSON.stringify(name)}] = c;
 }`;
+};
+
+function capitalize( str ) {
+	return str.charAt(0).toUpperCase() + str.substr(1).replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
 };
